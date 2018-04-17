32c673981406439f68e9d9e528a4e1761a240ab1
Calum Brier <c.brier9826@student.leedsbeckett.ac.uk>
2018-04-17 13:05:10 +0100

    Hello World, now works

---
+++ src/main/java/com/calum/guestbook/controller/GuestBookController.java
@@ -0,0 +1,16 @@
+package com.calum.guestbook.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GuestBookController {
+
+    @GetMapping ("/")
+    public String testMapping () {
+        return "Hello, World";
+    }
+
+}
+
