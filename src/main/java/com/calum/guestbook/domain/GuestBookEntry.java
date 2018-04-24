91814c066370d8f373aadedde191b7a0f40f8683
Calum Brier <c.brier9826@student.leedsbeckett.ac.uk>
2018-04-24 11:56:00 +0100

    Guest book entry
    Adding the GuestBookEntry package

--- src/main/java/com/calum/guestbook/domain/GuestBookEntry.java
+++ src/main/java/com/calum/guestbook/domain/GuestBookEntry.java
@@ -1,4 +1,23 @@
 package com.calum.guestbook.domain;
 
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+
+@Entity
 public class GuestBookEntry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @NotEmpty
+    private String user;
+    @NotEmpty
+    private String comment;
+
 }
---
+++ src/main/java/com/calum/guestbook/service/GuestBookService.java
@@ -0,0 +1,4 @@
+package com.calum.guestbook.service;
+
+public class GuestBookService {
+}
