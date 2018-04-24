0706edc4f3a5433fa32225dfa70095fc179cf75e
Calum Brier <c.brier9826@student.leedsbeckett.ac.uk>
2018-04-24 12:03:11 +0100

    GuestBookSerivce class
    This new class includes the first example of dependency injection we have used. The @Autowired
    annotation declares that an instance of GuestBookRepository is required by the service layer;
    this instance will be created automatically when the app is started.

--- src/main/java/com/calum/guestbook/service/GuestBookService.java
+++ src/main/java/com/calum/guestbook/service/GuestBookService.java
@@ -1,4 +1,19 @@
 package com.calum.guestbook.service;
 
+import com.calum.guestbook.domain.GuestBookEntry;
+import com.calum.guestbook.domain.GuestBookEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GuestBookService {
+
+    @Autowired
+    private GuestBookEntryRepository guestBookEntryRepository;
+
+    public List <GuestBookEntry> findAllEntries () {
+        return this.guestBookEntryRepository.findAll ();
+    }
 }
