aa590d60db3db23f71fe2135acae46ab95b0e9e5
Calum Brier <c.brier9826@student.leedsbeckett.ac.uk>
2018-04-24 12:11:35 +0100

    Connecting the serivce to the controller
    The controller class now
    requires an instance of the service class. This is dependency injection again, so we simply add an
    @Autowired declaration to the controller class:

--- src/main/java/com/calum/guestbook/controller/GuestBookController.java
+++ src/main/java/com/calum/guestbook/controller/GuestBookController.java
@@ -1,16 +1,24 @@
 package com.calum.guestbook.controller;
 
 
+import com.calum.guestbook.domain.GuestBookEntry;
+import com.calum.guestbook.service.GuestBookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GuestBookController {
 
     @GetMapping ("/")
-    public String testMapping () {
-        return "Hello, World";
+    public List<GuestBookEntry> testMapping () {
+        return guestBookService.findAllEntries();
     }
+
+    @Autowired
+    private GuestBookService guestBookService;
 
 }
 
