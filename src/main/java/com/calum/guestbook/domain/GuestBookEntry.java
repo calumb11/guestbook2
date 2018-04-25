3be331b368c010749f79b79cb4352a98c73ce16d
Calum Brier <c.brier9826@student.leedsbeckett.ac.uk>
2018-04-25 12:43:58 +0100

    Tidying up
    Changing the code to more developer friendly

--- src/main/java/com/calum/guestbook/domain/GuestBookEntry.java
+++ src/main/java/com/calum/guestbook/domain/GuestBookEntry.java
@@ -8,35 +8,55 @@ import java.util.List;
 
 
 @Entity
-@Table (name = "entries")
+@Table (name = "entires")
 public class GuestBookEntry {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "entry_id")
     private Integer id;
+
     @NotEmpty
     private String user;
+
     @NotEmpty
     private String comment;
 
+    public GuestBookEntry (String user, String comment) {
+        this.user = user;
+        this.comment = comment;
+    }
 
-    public Integer getId() {
+    public GuestBookEntry (Integer id, String user, String comment) {
+        this.id = id;
+        this.user = user;
+        this.comment = comment;
+    }
+
+
+    public Integer getId () {
         return id;
     }
-    public void setId(Integer id) {
+
+
+    public void setId (Integer id) {
         this.id = id;
     }
+
 
     public String getUser() {
         return user;
     }
-    public void setUser(String user) {
+
+    public void setUser (String user) {
         this.user = user;
     }
+
 
     public String getComment() {
         return comment;
     }
+
 
     public void setComment(String comment) {
         this.comment = comment;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO GUEST_BOOK_ENTRY (COMMENT, USER) VALUES ('Great Comment', 'john');
-INSERT INTO GUEST_BOOK_ENTRY (COMMENT, USER) VALUES ('Me Too!', 'jane');
-INSERT INTO GUEST_BOOK_ENTRY (COMMENT, USER) VALUES ('I agree.', 'alice');
-INSERT INTO GUEST_BOOK_ENTRY (COMMENT, USER) VALUES ('Of course!', 'john');
+INSERT INTO entries (COMMENT, USER) VALUES ('Great Comment', 'john');
+INSERT INTO entires (COMMENT, USER) VALUES ('Me Too!', 'jane');
+INSERT INTO entires (COMMENT, USER) VALUES ('I agree.', 'alice');
+INSERT INTO entries (COMMENT, USER) VALUES ('Of course!', 'john');
